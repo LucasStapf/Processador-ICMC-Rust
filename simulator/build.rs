6e2f34196466469d65381a7d0ae1b60911422c42
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2024-02-14 17:41:54 -0300

    Update build.rs

--- simulator/build.rs
+++ simulator/build.rs
@@ -1,7 +1,13 @@
+use std::env;
+
 fn main() {
     glib_build_tools::compile_resources(
         &["resources"],
         "resources/resources.gresource.xml",
         "compile.gresource",
     );
+
+    if env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("msvc") {
+        println!("cargo:rustc-link-arg=/stack:{}", 10 * 1024 * 1024);
+    }
 }
